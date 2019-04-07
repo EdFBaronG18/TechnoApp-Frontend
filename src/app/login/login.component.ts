3086fc38073517d1ed0658de03472ff53e9fdbbe
Andres Osorio <gd_andres98@hotmail.com>
2019-04-07 16:38:01 -0500

    Login component ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserInformationService } from '../services/user-information.service';
+import { User } from '../model/object';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +9,37 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  private username: String;
+  private password: String;
+  private message: String;
   hide = true;
   
-  constructor(public router: Router) { }
+  constructor(private servicioLogin: UserInformationService,public router: Router) {
+    if(servicioLogin.isLogged)
+    this.router.navigateByUrl("/artists");
+   }
 
   ngOnInit() {
+  }
+  
+  async login(){
+    let responseAutentication: any;
+    let user : User;
+    this.servicioLogin.autenticate(this.username, this.password).subscribe(
+      dataResponse => {
+        responseAutentication = dataResponse;
+          console.log(responseAutentication);
+          user = <User>responseAutentication;
+
+          if(user != null){
+              this.servicioLogin.loggin(JSON.stringify(user));
+              this.servicioLogin.fillUserInfo(user);
+              this.router.navigateByUrl("/artists");
+          }
+          else
+            this.message = "Usuario o contraseña incorrecta.";
+      }, 
+    );
   }
 
 }
