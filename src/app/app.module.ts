fa8a6cd8a5c16cfc45453869319563d7ce7e6a32
Edward-Acer <edwardbaron99@gmail.com>
2019-04-07 12:10:34 -0500

    Generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ArtistDetailComponent } from './artist-detail/artist-detail.component';
+import { ArtistListComponent } from './artist-list/artist-list.component';
+import { CommentDetailComponent } from './comment-detail/comment-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ArtistDetailComponent,
+    ArtistListComponent,
+    CommentDetailComponent
   ],
   imports: [
     BrowserModule,
