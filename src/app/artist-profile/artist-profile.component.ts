7e15cd6963617654304041f4890907b2a1323a9a
Edward-Acer <edwardbaron99@gmail.com>
2019-04-07 22:22:08 -0500

    Banner profile

--- src/app/artist-profile/artist-profile.component.ts
+++ src/app/artist-profile/artist-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-artist-profile',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artist-profile.component.css']
 })
 export class ArtistProfileComponent implements OnInit {
+
+  @Input() mySelectedArtist: Artist; 
 
   constructor() { }
 
