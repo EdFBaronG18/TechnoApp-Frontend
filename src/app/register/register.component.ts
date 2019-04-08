1cb0c461124e08b72d657cd1c66ed7e45f101c17
Edward-Acer <edwardbaron99@gmail.com>
2019-04-07 20:15:18 -0500

    Service register errors

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { User } from '../model/object';
-import { HttpParams } from '@angular/common/http';
-import { UserService } from '../services/user.service';
-import { UserInformationService } from '../services/user-information.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { User } from "../model/object";
+import { HttpParams } from "@angular/common/http";
+import { UserService } from "../services/user.service";
+import { UserInformationService } from "../services/user-information.service";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
   private passwordA: String;
@@ -16,59 +16,63 @@ export class RegisterComponent implements OnInit {
   private registerError: String;
   myUser: User;
   name: String;
-  
 
-  constructor(private userService: UserService,private userInfo: UserInformationService, public router: Router) {
+  constructor(
+    private userService: UserService,
+    public router: Router
+  ) {
     this.myUser = new User();
-    this.name = userInfo.getName();
-    this.myUser.username = userInfo.getUser().username;
   }
-  
-  ngOnInit() {
-  }
-  addNewTweet() {
+
+  ngOnInit() {}
+  addNewUser() {
     var good = true;
     this.registerError = "No se ha podido registrar al usuario:\n";
-    if(!this.validateUsername(this.myUser.username)){
-      this.registerError+= "- Nombre de usuario inválido\n";
+    if (!this.validateUsername(this.myUser.username)) {
+      this.registerError += "- Nombre de usuario inválido\n";
+      this.myUser.username = "";
       good = false;
     }
-    if(!this.validateName(this.myUser.name)){
-      this.registerError+= "- Nombre inválido\n";
+
+    if (!this.validateName(this.myUser.name)) {
+      this.registerError += "- Nombre inválido\n";
       good = false;
     }
-    if(this.passwordA != this.passwordB){
-      this.registerError+= "- Las contraseñas no coinciden\n";
+
+    if (this.passwordA != this.passwordB) {
+      this.registerError += "- Las contraseñas no coinciden\n";
       good = false;
-    }
-    else{
-      if(!this.validatePassword(this.passwordA)){
-        this.registerError+= "- Contraseña inválida\n";
+    } else {
+      if (!this.validatePassword(this.passwordA)) {
+        this.registerError += "- Contraseña inválida\n";
         good = false;
       }
     }
-    if(good){
-      this.userService.registerUser(this.myUser,this.passwordA);
+
+    if (good) {
+      this.userService.registerUser(this.myUser, this.passwordA);
       this.myUser = new User();
-    }
-    else
+    } else {
       alert(this.registerError);
-    
+      this.passwordA = this.passwordB = "";
+    }
   }
 
-  private validateUsername( username : String):boolean {
-    if(username.length==0||!this.userService.userDontExist(username))
+  private validateUsername(username: String){
+    
+    var x = this.userService.userDontExist(username);
+    console.log(x);
+    
+    if (username.length == 0 || x != null)
       return false;
     return true;
   }
 
-  private validateName( name : String):boolean {
-      return name.length!=0;
+  private validateName(name: String): boolean {
+    return name.length != 0;
   }
 
-  private validatePassword( pass : String):boolean {
-    return pass.length < 3;
-}
-
-
+  private validatePassword(pass: String): boolean {
+    return pass.length > 3;
+  }
 }
